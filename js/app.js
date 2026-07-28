69a5382f5876e8dbd7e4588b3a4c3da54b4125af
Moochi_bazar <hasan99999id@gmail.com>
2026-07-28 12:41:26 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -93,18 +93,15 @@ function animateCards() {
     cards.forEach((card, index) => {
 
         card.style.opacity = "0";
-
-        card.style.transform = "translateY(30px)";
+        card.style.transform = "translateY(20px)";
 
         setTimeout(() => {
 
-            card.style.transition = ".5s";
-
+            card.style.transition = "0.4s";
             card.style.opacity = "1";
-
             card.style.transform = "translateY(0)";
 
-        }, index * 100);
+        }, index * 80);
 
     });
 
