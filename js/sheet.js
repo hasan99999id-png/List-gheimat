f12a22b2b6080a4b101a9a81c482a511ff3d0c91
Moochi_bazar <hasan99999id@gmail.com>
2026-07-28 12:23:42 +0330

    Update sheet.js

--- js/sheet.js
+++ js/sheet.js
@@ -1,134 +1,40 @@
-// =========================
-// تنظیمات
-// =========================
-
-// فقط این را تغییر بده
 const SHEET_ID = "1OImD12FCHFiDxzN-h8lBWox2gM2jCmaAntPI_NjfWsg";
-
-// شماره برگه
 const GID = "0";
 
-// خودکار لینک ساخته می‌شود
-const SHEET_URL =
-`https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:json&gid=${GID}`;
+const URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?gid=${GID}`;
 
 let products = [];
 
-async function loadProducts(){
+fetch(URL)
+  .then(res => res.text())
+  .then(rep => {
 
-    try{
+    const data = JSON.parse(rep.substring(47).slice(0, -2));
 
-        document.getElementById("status").innerHTML="در حال دریافت اطلاعات...";
+    const rows = data.table.rows;
 
-        const res = await fetch(SHEET_URL);
+    rows.forEach(r => {
 
-        const text = await res.text();
-
-        const json = JSON.parse(text.substr(47).slice(0,-2));
-
-        const rows = json.table.rows;
-
-        products=[];
-
-        rows.forEach(row=>{
-
-            if(!row.c) return;
-
-            products.push({
-
-                code:row.c[0]?.v || "",
-
-                name:row.c[1]?.v || "",
-
-                price:row.c[2]?.v || ""
-
-            });
-
-        });
-
-        renderProducts(products);
-
-        document.getElementById("status").innerHTML=
-        products.length+" کالا";
-
-        hideLoader();
-
-    }
-
-    catch(error){
-
-        console.log(error);
-
-        document.getElementById("status").innerHTML=
-        "خطا در اتصال به Google Sheets";
-
-        hideLoader();
-
-    }
-
-}
-
-function renderProducts(list){
-
-    const box=document.getElementById("products");
-
-    box.innerHTML="";
-
-    list.forEach(item=>{
-
-        box.innerHTML+=`
-
-<div class="card">
-
-<img src="https://placehold.co/400x250?text=Auto+Part">
-
-<div class="card-body">
-
-<h3>${item.name}</h3>
-
-<div class="code">
-
-کد کالا :
-${item.code}
-
-</div>
-
-<div class="price">
-
-${Number(item.price).toLocaleString("fa-IR")} تومان
-
-</div>
-
-<a class="btn" href="#">
-
-مشاهده
-
-</a>
-
-</div>
-
-</div>
-
-`;
+      products.push({
+        code: r.c[0] ? r.c[0].v : "",
+        name: r.c[1] ? r.c[1].v : "",
+        price: r.c[2] ? r.c[2].v : ""
+      });
 
     });
 
-}
+    renderProducts(products);
 
-function hideLoader(){
+    document.getElementById("status").innerHTML =
+      products.length + " کالا";
 
-    const loader=document.getElementById("loader");
+    document.getElementById("loader").style.display = "none";
 
-    if(loader){
+  })
+  .catch(err => {
 
-        loader.style.display="none";
+    console.log(err);
 
-    }
+    document.getElementById("status").innerHTML = err;
 
-}
-
-window.onload=()=>{
-
-    loadProducts();
-
-};
+  });
